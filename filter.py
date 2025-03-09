aa39f9b46660373204968e5e79a5934b5a9462d3
nisof-ganteng <157976818+nisof-ganteng@users.noreply.github.com>
2025-03-09 08:56:23 +0700

    Update filter.py
    update multi filtering

--- filter.py
+++ filter.py
@@ -1,9 +1,9 @@
-def filter_domains(input_file, output_file, domain_type):
+def filter_domains(input_file, output_file, domain_types):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         buffer = []
         for line in infile:
             domain = line.strip()
-            if domain.endswith(domain_type):
+            if any(domain.endswith(domain_type) for domain_type in domain_types):
                 buffer.append(domain + '\n')
         
         outfile.writelines(buffer)
@@ -11,5 +11,6 @@ def filter_domains(input_file, output_file, domain_type):
 if __name__ == "__main__":
     input_file = input("List Domain: ")
     output_file = input("Result: ")
-    domain_type = input("Domain: ")
-    filter_domains(input_file, output_file, domain_type)
+    domain_types = input("Domain (pisahkan dengan koma): ").split(',')
+    domain_types = [domain.strip() for domain in domain_types]
+    filter_domains(input_file, output_file, domain_types)
